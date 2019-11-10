f47e47e06b5016a6c66cb95dd2219ecac0566a59
Team Dino <teamdino@example.com>
2019-11-10 11:21:46 +0100

    primo commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/daki/Desktop/65339_65338_65434_65322_ScrumBoard/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
